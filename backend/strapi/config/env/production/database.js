7fc4486863d1ea08314f93fabf27ec590cd70e56
willglopes <willg.lopes@gmail.com>
2025-07-21 19:30:44 +0100

    Switch to PG variables for database connection

--- backend/strapi/config/env/production/database.js
+++ backend/strapi/config/env/production/database.js
@@ -1,24 +1,14 @@
-module.exports = ({ env }) => {
-  console.log('=== DATABASE CONFIG DEBUG ===');
-  console.log('DATABASE_HOST:', env('DATABASE_HOST'));
-  console.log('DATABASE_NAME:', env('DATABASE_NAME'));
-  console.log('DATABASE_USER:', env('DATABASE_USER'));
-  console.log('DATABASE_PORT:', env('DATABASE_PORT'));
-  console.log('DATABASE_PASSWORD exists:', !!env('DATABASE_PASSWORD'));
-  console.log('==============================');
-  
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USER'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false)
-      },
-      debug: false,
+      host: env('PGHOST'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE'),
+      user: env('PGUSER'),
+      password: env('PGPASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false)
     },
-  };
-};
+    debug: false,
+  },
+});
