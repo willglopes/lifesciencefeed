59956cef6636dac9fe44b53a423adab415b4d44f
willglopes <willg.lopes@gmail.com>
2025-07-21 18:59:49 +0100

    Fix database config to use Railway DATABASE_ variables

--- backend/strapi/config/env/production/database.js
+++ backend/strapi/config/env/production/database.js
@@ -2,14 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST'),
-      port: env.int('PGPORT'),
-      database: env('PGDATABASE'),
-      user: env('PGUSER'),
-      password: env('PGPASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false) && {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-      },
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USER'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false)
     },
     debug: false,
   },
