0de9fac310a5efea2f119330e2478e2fa199ab8a
willglopes <willg.lopes@gmail.com>
2025-07-22 16:42:08 +0100

    server updates

--- backend/strapi/config/server.ts
+++ backend/strapi/config/server.ts
@@ -1,7 +1,8 @@
-module.exports = ({ env } ) => ({
+// backend/strapi/config/env/production/server.js
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8080),
-  url: env('PUBLIC_URL', 'https://lifesciencefeed.railway.app' ),
+  url: env('PUBLIC_URL'),
 });
 
 
