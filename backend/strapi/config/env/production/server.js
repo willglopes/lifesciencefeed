85483f415cbab9ff10df25160beb39e93d3a8c50
willglopes <willg.lopes@gmail.com>
2025-07-22 16:46:37 +0100

    updates

--- backend/strapi/config/env/production/server.js
+++ backend/strapi/config/env/production/server.js
@@ -1,6 +1,10 @@
-// backend/strapi/config/env/production/server.js
+// backend/strapi/config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8080),
   url: env('PUBLIC_URL'),
+  app: {
+    // Parse the comma‑separated APP_KEYS into an array
+    keys: env.array('APP_KEYS'),
+  },
 });
